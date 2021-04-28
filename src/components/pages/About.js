192b72e1c8e79cd662502061bd30fe899fee6840
RenatoMignella <renatoferigato@gmail.com>
2021-04-29 00:52:59 +0100

    pushing app

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+export const About = () => {
+   return (
+      <Fragment>
+         <h1>About this amazing app</h1>
+         <p>App to Search Github Users</p>
+         <p>Version 0.001</p>
+      </Fragment>
+   );
+};
+export default About;
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const NotFound = () => {
+   return (
+      <div>
+         <h1> Not Found </h1>
+
+         <p className='lead'> The page that you're searching wasn't found..... </p>
+      </div>
+   );
+};
+
+export default NotFound;
